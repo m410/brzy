1bfb6a236356550e20860ddf0197163a0aed9433
Michael Fortin <michael@m410.us>
2010-08-02 09:29:56 -0400

    removed examples

--- mods/brzy-jpa/src/main/scala/org/brzy/jpa/build/PersistenceXml.scala
+++ mods/brzy-jpa/src/main/scala/org/brzy/jpa/build/PersistenceXml.scala
@@ -9,14 +9,13 @@ import transform.{RewriteRule, RuleTransformer}
  * Document Me..
  * 
  * @author Michael Fortin
- * @version $Id: $
  */
 class PersistenceXml(config:BootConfig) {
   private val template = XML.load(getClass.getClassLoader.getResource("template.persistence.xml"))
   private val children = ListBuffer[Elem]()
   private val parentName = "persistence-unit"
   
- 
+  // TODO Add children, classes and properties from the configuration
 
   val body = new RuleTransformer(new AddChildrenTo(parentName, children)).transform(template).head
 
