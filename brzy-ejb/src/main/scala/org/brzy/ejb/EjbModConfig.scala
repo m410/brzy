a5111c5303632d2e9fa42a7951a4081043793046
Michael Fortin <michael@m410.us>
2010-08-11 18:09:25 -0400

    Got ejb unit tests working

--- brzy-ejb/src/main/scala/org/brzy/ejb/EjbModConfig.scala
+++ brzy-ejb/src/main/scala/org/brzy/ejb/EjbModConfig.scala
@@ -1,6 +1,9 @@
 package org.brzy.ejb
 
 import org.brzy.config.mod.Mod
+import collection.mutable.ListBuffer
+import java.lang.String
+import collection.immutable.Map
 
 /**
  * Document Me..
@@ -11,14 +14,27 @@ class EjbModConfig(map: Map[String, AnyRef]) extends Mod(map) {
   override val configurationName = "Ejb Configuration"
   val jndi: Option[String] = map.get("jndi").asInstanceOf[Option[String]].orElse(None)
 
-  val beans:List[EjbBean] = Nil
-  
+  val beans: List[EjbBean] = {
+    if (map.contains("beans")) {
+      val bs = map("beans")
+      println("bs: " + bs)
+      val buf = ListBuffer[EjbBean]()
+      bs.asInstanceOf[List[Map[String, String]]].foreach((b: Map[String, String]) => {
+        buf += EjbBean(b("service_name"), b("remote_interface"), b("jndi_name"))
+      })
+      buf.toList
+    }
+    else {
+      Nil
+    }
+  }
+
   /*
-  beans:
-    - service_name: myService
-      remote_interface: com.somecompany.service.MyServiceRemote
-      jndi_name: java:comp/env/myservice
-   */
+ beans:
+   - service_name: myService
+     remote_interface: com.somecompany.service.MyServiceRemote
+     jndi_name: java:comp/env/myservice
+  */
   override def <<(that: Mod): Mod = {
     if (that == null) {
       this
--- brzy-ejb/src/test/scala/org/brzy/ejb/EjbServiceTest.scala
+++ brzy-ejb/src/test/scala/org/brzy/ejb/EjbServiceTest.scala
@@ -1,28 +1,29 @@
 package org.brzy.ejb
 
+import mock.MyBeanServiceImpl
 import org.scalatest.junit.JUnitSuite
 import org.junit.Test
 import org.junit.Assert._
-import javax.naming.{InitialContext, Context}
+import org.springframework.mock.jndi.SimpleNamingContextBuilder
 
 class EjbServiceTest extends JUnitSuite {
-  System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory")
-  System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming")
-  
-  val ic = new InitialContext()
-  ic.createSubcontext("java:")
-  ic.createSubcontext("java:/comp")
-  ic.createSubcontext("java:/comp/env")
-  ic.bind("java:/comp/env/MyBean", new MyBean)
+  val builder = new SimpleNamingContextBuilder
+  builder.bind("java:/comp/env/MyBeanService", new MyBeanServiceImpl)
+  builder.activate
 
   @Test def testLookup = {
-    val config = new EjbModConfig(Map[String,AnyRef]())
+    val config = new EjbModConfig(Map[String,AnyRef](
+      "name" -> "EJB",
+      "beans" -> List(
+          Map(
+            "service_name" -> "myBeanService",
+            "remote_interface" -> "org.brzy.ejb.mock.MyBeanService",
+            "jndi_name" -> "java:/comp/env/MyBeanService")
+        )
+      ))
     val provider = new EjbModProvider(config)
     assertNotNull(provider.serviceMap)
     assertEquals(1, provider.serviceMap.size)
   }
 }
 
-class MyBean {
-  def sayHello = "Hello"
-}
