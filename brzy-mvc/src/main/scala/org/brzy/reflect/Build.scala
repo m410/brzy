91b6d76c1d95b1a60b79d14bd424ea6dceaa865d
Michael Fortin <michael@m410.us>
2010-08-28 11:54:27 -0400

    added builder and unit test

--- brzy-mvc/src/main/scala/org/brzy/reflect/Build.scala
+++ brzy-mvc/src/main/scala/org/brzy/reflect/Build.scala
@@ -1,21 +1,33 @@
 package org.brzy.reflect
 
 import collection.mutable.ArrayBuffer
+import java.beans.ConstructorProperties
 
 /**
  * Document Me..
  * 
  * @author Michael Fortin
  */
-class Build[T]()(implicit m:Manifest[T]) {
-  private[this] val cls = m.erasure
-  private[this] val args = ArrayBuffer[Any]()
+object Build {
 
-  def arg(name:String, value:Any) = {
-    this
-  }
+  def apply[T]()(implicit m: Manifest[T]) = new Builder[T]()
 
-  def make:T = {
-    cls.newInstance.asInstanceOf[T]
+  class Builder[T]()(implicit m: Manifest[T]) {
+    private[this] val cls = m.erasure
+    private[this] val argNames =  cls.getAnnotation(classOf[ConstructorProperties]).value
+    private[this] val constructor =
+        cls.getConstructors.find(c=> c.getParameterTypes.length == argNames.length) match {
+          case Some(a) => a
+          case _ => error("No matching Constructor found for ConstructorProperties annotation.")
+        }
+
+    private[this] val args = new ArrayBuffer[java.lang.Object](constructor.getParameterTypes.length)
+
+    def arg(a:(String, java.lang.Object)) = {
+      args.insert(argNames.indexOf(a._1),a._2)
+      this
+    }
+
+    def make:T = constructor.newInstance(args.toArray:_*).asInstanceOf[T]
   }
 }
---
+++ brzy-mvc/src/test/scala/org/brzy/reflect/BuildTest.scala
@@ -0,0 +1,22 @@
+package org.brzy.reflect
+
+import org.scalatest.junit.JUnitSuite
+import org.junit.Test
+import org.junit.Assert._
+import java.beans.ConstructorProperties
+
+
+class BuildTest extends JUnitSuite {
+  @Test def testBuild = {
+    val fixture:BuildFixture = Build[BuildFixture]()
+            .arg("name"->"test")
+            .arg("num"->4.asInstanceOf[AnyRef])
+            .make
+    assertNotNull(fixture)
+    assertEquals("test",fixture.name)
+    assertEquals(4,fixture.num)
+  }
+}
+
+@ConstructorProperties(Array("name","num"))
+class BuildFixture(val name:String, val num:Int)
