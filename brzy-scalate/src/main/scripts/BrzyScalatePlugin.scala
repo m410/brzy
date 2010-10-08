28919d7a332bb7c16d47aeedddfd2f659cac1348
Michael Fortin <michael@m410.us>
2010-10-07 22:50:37 -0400

    Updated scalate module to use updated fab build, updated pom to set version numbers.

--- brzy-scalate/src/main/scripts/BrzyScalatePlugin.scala
+++ brzy-scalate/src/main/scripts/BrzyScalatePlugin.scala
@@ -1,10 +1,25 @@
+import java.io.{BufferedWriter, FileWriter}
+import org.clapper.scalasti.StringTemplateGroup
+import org.brzy.fab.build.BuildContext
 
+class BrzyScalatePlugin(context:BuildContext)  {
 
-@Plugin(name="scalate-plugin", desc="Generate SSP view pages")
-class BrzyScalatePlugin(context:BuildContext) extends BuildPlugin(context) {
-
-	@Task(name="gen-ssp-views",desc="Create CRUD views with Scalate")
 	def genSspViews(args:Array[String]) = {
 		context.line.say(Info("Generate views for domain"))
+
+    // todo check for class name arg, if not ask for it
+    // load it, get the properties
+
+    val group = new StringTemplateGroup("brzy", File(brzyHome, ".brzy/modules/brzy-scalate/templates"))
+
+
+    val viewTemplate = group.template("view-ssp")
+    // todo set attributes
+    viewTemplate.setAttribute("name", name)
+    val viewFile: Any = File(name + "/webapp/" ++ "/HomeController.scala")
+    // todo check if it exists, and ask to overwrite
+    val writer = new BufferedWriter(new FileWriter(viewFile))
+    writer.write(viewTemplate.toString)
+    writer.close
 	}
 }
