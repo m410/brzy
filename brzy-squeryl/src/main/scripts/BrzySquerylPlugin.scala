6350527102cf50cde85d8a46082d411c1d2fa725
Michael Fortin <michael@m410.us>
2010-10-11 23:46:59 -0400

    Update squeryl build plugin so it will now create domain classes.

--- brzy-squeryl/src/main/scripts/BrzySquerylPlugin.scala
+++ brzy-squeryl/src/main/scripts/BrzySquerylPlugin.scala
@@ -1,18 +1,28 @@
 
+import java.io.{BufferedWriter, FileWriter}
+import org.brzy.fab.print.{Question,Info}
+import org.clapper.scalasti.StringTemplateGroup
+import org.brzy.fab.file.{Files, File}
+import org.brzy.fab.file.FileUtils._
+import org.brzy.fab.build.BuildContext
 
-class BrzySquerylPlugin(context:BuildContext) extends BuildPlugin(context) {
+class BrzySquerylPlugin(context:BuildContext)  {
 
 	def createSquerylDomain(args:Array[String]) = {
 		context.line.say(Info("Create a Squeryl Domain Class"))
-		// ask for package name & class name
+		val packageAndClass = context.line.ask(Question("enter package & class: "))
+		val className = packageAndClass.substring(packageAndClass.lastIndexOf(".") +1)
+    val packageName = packageAndClass.substring(0,packageAndClass.lastIndexOf("."))
 		// ask if you want to add the controller
 		// ask to create unit test
-//		val group = new StringTemplateGroup("mygroup", File(".brzy/modules/brzy-squeryl/templates"))
-//		val template = group.template("mytemplate")
-//		template.setAttribute("name","value")
-//		val outputFile = File("")
-//		val writer = new BufferedWriter(new FileWriter(outputFile))
-//		writer.write(template.toString)
-//		writer.close
+
+		val group = new StringTemplateGroup("mygroup", File(".brzy/modules/brzy-squeryl/templates"))
+		val template = group.template("domain-scala")
+		template.setAttribute("packageName",packageName)
+		template.setAttribute("className",className)
+		val outputFile = File("src/scala/" + className + ".scala")
+		val writer = new BufferedWriter(new FileWriter(outputFile))
+		writer.write(template.toString)
+		writer.close
 	}
 }
