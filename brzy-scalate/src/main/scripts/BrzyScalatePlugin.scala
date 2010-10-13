4e4411135a37c08cf2e71d75b6c55fe0042a9038
Michael Fortin <michael@m410.us>
2010-10-12 22:24:21 -0400

    Fixed build plugin, updated initial templates.

--- brzy-scalate/src/main/scripts/BrzyScalatePlugin.scala
+++ brzy-scalate/src/main/scripts/BrzyScalatePlugin.scala
@@ -1,25 +1,60 @@
 import java.io.{BufferedWriter, FileWriter}
+import org.brzy.fab.print.{Question,Info}
 import org.clapper.scalasti.StringTemplateGroup
+import org.brzy.fab.file.{Files, File}
+import org.brzy.fab.file.FileUtils._
 import org.brzy.fab.build.BuildContext
 
 class BrzyScalatePlugin(context:BuildContext)  {
 
 	def genSspViews(args:Array[String]) = {
 		context.line.say(Info("Generate views for domain"))
+		val packageAndClass = context.line.ask(Question("enter package & class: "))
+		val className = packageAndClass.substring(packageAndClass.lastIndexOf(".") +1)
+    val packageName = packageAndClass.substring(0,packageAndClass.lastIndexOf("."))
+    val name = className.substring(0,1).toLowerCase + className.substring(1)
 
-    // todo check for class name arg, if not ask for it
-    // load it, get the properties
+		val templateDir = File(".brzy/modules/brzy-scalate/templates")
+    val group = new StringTemplateGroup("brzy", templateDir)
+    val outdir = File("webapp/" + name)
 
-    val group = new StringTemplateGroup("brzy", File(brzyHome, ".brzy/modules/brzy-scalate/templates"))
-
+    if(!outdir.exists)
+       outdir.mkdirs
 
     val viewTemplate = group.template("view-ssp")
-    // todo set attributes
-    viewTemplate.setAttribute("name", name)
-    val viewFile: Any = File(name + "/webapp/" ++ "/HomeController.scala")
-    // todo check if it exists, and ask to overwrite
-    val writer = new BufferedWriter(new FileWriter(viewFile))
-    writer.write(viewTemplate.toString)
-    writer.close
+    // viewTemplate.setAttribute("name", name)
+    viewTemplate.setAttribute("attributeName", name)
+    viewTemplate.setAttribute("fullClassName", packageAndClass)
+    val viewFile = File(outdir, "view.ssp")
+    val writer1 = new BufferedWriter(new FileWriter(viewFile))
+    writer1.write(viewTemplate.toString)
+    writer1.close
+
+    val createTemplate = group.template("create-ssp")
+    // createTemplate.setAttribute("name", name)
+    createTemplate.setAttribute("attributeName", name)
+    createTemplate.setAttribute("fullClassName", packageAndClass)
+    val createFile = File(outdir, "create.ssp")
+    val writer2 = new BufferedWriter(new FileWriter(createFile))
+    writer2.write(createTemplate.toString)
+    writer2.close
+
+    val editTemplate = group.template("edit-ssp")
+    // editTemplate.setAttribute("name", name)
+    editTemplate.setAttribute("attributeName", name)
+    editTemplate.setAttribute("fullClassName", packageAndClass)
+    val editFile = File(outdir, "edit.ssp")
+    val writer3 = new BufferedWriter(new FileWriter(editFile))
+    writer3.write(editTemplate.toString)
+    writer3.close
+
+    val listTemplate = group.template("list-ssp")
+    listTemplate.setAttribute("shortClassName", className)
+    listTemplate.setAttribute("fullClassName", packageAndClass)
+    listTemplate.setAttribute("attributeName", name)
+    val listFile = File(outdir, "list.ssp")
+    val writer4 = new BufferedWriter(new FileWriter(listFile))
+    writer4.write(listTemplate.toString)
+    writer4.close
 	}
 }
