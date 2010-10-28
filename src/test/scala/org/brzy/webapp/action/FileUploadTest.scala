05007bb90d195916f12bdbf15020c4e480d2c6f2
Michael Fortin <michael@m410.us>
2010-10-28 19:53:31 -0400

    Added PostBody constructor arg.

---
+++ src/test/scala/org/brzy/webapp/action/FileUploadTest.scala
@@ -0,0 +1,87 @@
+package org.brzy.webapp.action
+
+import args.PostBody
+import org.scalatest.junit.JUnitSuite
+import org.junit.Assert._
+import java.io.FileInputStream
+import xml.Elem
+import org.springframework.mock.web.{MockHttpServletRequest, MockMultipartHttpServletRequest, MockMultipartFile, MockHttpServletResponse}
+import io.Source
+import org.junit.{Ignore, Test}
+
+/**
+ * Document Me..
+ *
+ * @author Michael Fortin
+ * @version $Id : $
+ */
+
+class FileUploadTest extends JUnitSuite {
+  val boundary = "AaB03x"
+  val content = "myContent1\n" + boundary
+  val contentJson = """{"key":"value"}"""
+  val contentXml = """<something>some value</something>"""
+
+  @Test @Ignore def testUploadbytes = {
+    val request = new MockMultipartHttpServletRequest()
+
+    request.setMethod("POST")
+    request.setContentType("multipart/form-data; boundary=" + boundary)
+    request.addHeader("Content-type", "multipart/form-data")
+    request.addHeader("Content-length", content.getBytes.length.toString)
+    request.addHeader("Content-Disposition", "form-data; name=\"submit-name\"")
+    request.addParameter("key","val")
+    request.setRequestURI("/files/upload")
+
+    val file = new MockMultipartFile("file", "/home/some/image.png", "application/png", content.getBytes)
+    request.addFile(file)
+
+    println(request.getFileMap)
+    val postbody = new PostBody(request)
+    val bytes = postbody.asBytes("file")
+    assertNotNull(bytes)
+    //    assertEquals(content,text)
+  }
+
+  @Test def testUploadText = {
+    val request = new MockMultipartHttpServletRequest()
+    request.setMethod("POST")
+    request.addHeader("Content-length", content.getBytes.length.toString)
+    request.addHeader("Content-Disposition", "form-data; name=\"submit-name\"")
+    request.setContent(content.getBytes)
+
+    request.setRequestURI("/files/upload")
+    val postbody = new PostBody(request)
+    val text = postbody.asText
+    assertNotNull(text)
+    assertEquals(content, text)
+  }
+
+  @Test def testUploadJson = {
+    val request = new MockMultipartHttpServletRequest()
+    request.setMethod("POST")
+    request.addHeader("Content-length", contentJson.getBytes.length.toString)
+    request.addHeader("Content-Disposition", "form-data; name=\"submit-name\"")
+    request.setContent(contentJson.getBytes)
+
+    request.setRequestURI("/files/upload")
+    val postbody = new PostBody(request)
+    val json = postbody.asJson
+    assertNotNull(json)
+    assertTrue(json.isInstanceOf[Map[_, _]])
+  }
+
+  @Test def testUploadXml = {
+    val request = new MockMultipartHttpServletRequest()
+    request.setMethod("POST")
+    request.addHeader("Content-length", contentXml.getBytes.length.toString)
+    request.addHeader("Content-Disposition", "form-data; name=\"submit-name\"")
+    request.setContent(contentXml.getBytes)
+
+    request.setRequestURI("/files/upload")
+    val postbody = new PostBody(request)
+    val xml = postbody.asXml
+    assertNotNull(xml)
+    assertTrue(xml.isInstanceOf[Elem])
+  }
+}
