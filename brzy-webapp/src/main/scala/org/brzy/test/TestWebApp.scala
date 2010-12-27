858d2ad0ef10c75e5002b4dc4b16dc7539381505
Michael Fortin <michael@m410.us>
2010-12-26 21:15:22 -0500

    Added test helper

---
+++ brzy-webapp/src/main/scala/org/brzy/test/TestWebApp.scala
@@ -0,0 +1,21 @@
+package org.brzy.test
+
+import org.brzy.application.WebApp
+
+/**
+ * Document Me..
+ * 
+ * @author Michael Fortin
+ * @version $Id: $
+ */
+
+object TestWebApp {
+//  def apply = {
+//    val config =
+//      if() // first check the user directory
+//      else // check classpath for brzy-webapp.test.b.yml
+//
+//
+//    WebApp(confg)
+//  }
+}
