0bc55100a8e2b1157dc168f75bab2bbd2e8484c0
Michael Fortin <michael@m410.us>
2011-01-16 16:16:09 -0500

    Added security functions to scalate views

---
+++ brzy-scalate/src/main/scala/org/brzy/mod/scalate/view/SecureFunctions.scala
@@ -0,0 +1,29 @@
+package org.brzy.mod.scalate.view
+
+import javax.servlet.http.HttpServletRequest
+import org.brzy.webapp.action.args.Principal
+
+/**
+ * Document Me..
+ * 
+ * @author Michael Fortin
+ */
+object SecureFunctions {
+  def isAuthorized(roles:String*)(implicit req:HttpServletRequest):Boolean =
+      if(req.getSession(false) == null) {
+        false
+      }
+      else {
+        val principal = req.getSession(false).getAttribute("brzy_principal").asInstanceOf[Principal]
+        principal.roles.allowed.forall(role=>roles.contains(role))
+      }
+
+  def isLoggedIn()(implicit req:HttpServletRequest):Boolean =
+      if(req.getSession(false) == null) {
+        false
+      }
+      else {
+        req.getSession(false).getAttribute("brzy_principal") != null
+      }
+
+}
