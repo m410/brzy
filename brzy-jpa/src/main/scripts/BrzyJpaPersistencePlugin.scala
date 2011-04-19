1f7fcb531075521313842fe7dc19e84c8a55a40f
Michael Fortin <michael@m410.us>
2011-04-18 23:36:32 -0400

    Fixed build tasks

--- brzy-jpa/src/main/scripts/BrzyJpaPersistencePlugin.scala
+++ brzy-jpa/src/main/scripts/BrzyJpaPersistencePlugin.scala
@@ -4,13 +4,12 @@ import org.brzy.mod.jpa.build.PersistenceXml
 import org.brzy.application.WebAppConf
 import org.brzy.fab.build.Task
 
-class BrzyJpaPersistencePlugin extends Task  {
-
+class BrzyJpaPersistencePlugin extends Task {
   // create a class from template
 	def genPersistenceXml() {
 		messenger.info("Create a JPA Domain Class")
-    val webappConf = WebAppConf.fromJson(context.property("webAppConfig"))
-    val outFile = File(context.webappDir, "WEB-INF/classes/META-INF/persistence.xml")
+    val webappConf = WebAppConf.fromJson(configuration.property("webAppConfig"))
+    val outFile = File(configuration.webappDir, "WEB-INF/classes/META-INF/persistence.xml")
     outFile.getParentFile.mkdirs
     val persistenceXml = new PersistenceXml(webappConf)
     persistenceXml.saveToFile(outFile.getAbsolutePath)
