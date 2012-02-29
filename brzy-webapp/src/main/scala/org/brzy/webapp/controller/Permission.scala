39bedab6daabb01bd57e6a2e1557589ed280c257
Michael Fortin <michael@m410.us>
2012-02-29 08:23:34 -0500

    comment

--- brzy-webapp/src/main/scala/org/brzy/webapp/controller/Permission.scala
+++ brzy-webapp/src/main/scala/org/brzy/webapp/controller/Permission.scala
@@ -25,29 +25,29 @@ import org.brzy.webapp.action.args.PrincipalSession
  * @author Michael Fortin
  */
 trait Permission[T<:Authenticated] {
+
+  // TODO document me
+  /**
+   *
+   * @return
+   */
 	def authenticator:Authenticator[T]
-		
-	protected[controller] val HEX_CHAR_TABLE = Array(
-	    '0'.asInstanceOf[Byte], '1'.asInstanceOf[Byte], '2'.asInstanceOf[Byte], '3'.asInstanceOf[Byte],
-	    '4'.asInstanceOf[Byte], '5'.asInstanceOf[Byte], '6'.asInstanceOf[Byte], '7'.asInstanceOf[Byte],
-	    '8'.asInstanceOf[Byte], '9'.asInstanceOf[Byte], 'a'.asInstanceOf[Byte], 'b'.asInstanceOf[Byte],
-	    'c'.asInstanceOf[Byte], 'd'.asInstanceOf[Byte], 'e'.asInstanceOf[Byte], 'f'.asInstanceOf[Byte])
 
-	protected[controller] def encodeHex(data:Array[Byte]) = {
-		val hex = new Array[Byte](2 * data.length)
-    var index:Int = 0
+  // TODO document me
+  /**
+   *
+   * @param person
+   * @return
+   */
+  def active(person:T):Boolean
 
-    for (b <- data) {
-      val v:Int = b & 0xFF;
-
-      hex(index) = HEX_CHAR_TABLE(v >>> 4)
-			index=index+1
-      hex(index) = HEX_CHAR_TABLE(v & 0xF)
-			index=index+1
-    }
-    new String(hex, "UTF8")
-	}
-
+  // TODO document me
+  /**
+   *
+   * @param userName
+   * @param password
+   * @return
+   */
 	def login(userName:String, password:String):Option[T] = authenticator.login(userName,encrypt(password))
 
 	def encrypt(password:String):String = {
@@ -59,5 +59,27 @@ trait Permission[T<:Authenticated] {
 		PrincipalSession(t.userName,t.authenticatedRoles)
 	}
 
-  def active(person:T):Boolean
+
+  private[this] val HEX_CHAR_TABLE = Array(
+    '0'.asInstanceOf[Byte], '1'.asInstanceOf[Byte], '2'.asInstanceOf[Byte], '3'.asInstanceOf[Byte],
+    '4'.asInstanceOf[Byte], '5'.asInstanceOf[Byte], '6'.asInstanceOf[Byte], '7'.asInstanceOf[Byte],
+    '8'.asInstanceOf[Byte], '9'.asInstanceOf[Byte], 'a'.asInstanceOf[Byte], 'b'.asInstanceOf[Byte],
+    'c'.asInstanceOf[Byte], 'd'.asInstanceOf[Byte], 'e'.asInstanceOf[Byte], 'f'.asInstanceOf[Byte])
+
+  private[this] def encodeHex(data:Array[Byte]) = {
+    val hex = new Array[Byte](2 * data.length)
+    var index:Int = 0
+
+    for (b <- data) {
+      val v:Int = b & 0xFF;
+
+      hex(index) = HEX_CHAR_TABLE(v >>> 4)
+      index=index+1
+      hex(index) = HEX_CHAR_TABLE(v & 0xF)
+      index=index+1
+    }
+    new String(hex, "UTF8")
+  }
+
+
 }
